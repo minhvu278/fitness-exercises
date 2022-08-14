613d91ebc1bde6572e96774720317e42dcd3cf10
vudo <dominhvu278@gmail.com>
2022-08-14 22:41:41 +0700

    done navbar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,16 @@
 import React from "react";
+import {Box} from "@mui/material";
+import HeroBanner from "../components/HeroBanner";
+import SearchExercise from "../components/SearchExercise";
+import Exercise from "../components/Exercise";
 
 const Home = () => {
     return (
-        <div>
-            Home
-        </div>
+        <Box>
+            <HeroBanner />
+            <SearchExercise />
+            <Exercise />
+        </Box>
     );
 }
 
